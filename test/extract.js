b9146d862be18e9fe1e2a5a4cc01fbe03cbc4ed5
coderaiser <coderaiser@cloudcmd.io>
2021-01-05 14:17:18 +0200

    fix(onezip) entry goes file before directory (#7)

--- test/extract.js
+++ test/extract.js
@@ -16,22 +16,28 @@ const {
 } = require('fs');
 
 const rimraf = require('rimraf');
-const test = require('supertape');
+const {test, stub} = require('supertape');
 const wait = require('@iocmd/wait');
 
+const mockRequire = require('mock-require');
+
 const {extract} = require('..');
+
+const {reRequire, stopAll} = mockRequire;
 
 const fixtureZip = () => join(__dirname, 'fixture', 'onezip.txt.zip');
 const tmp = () => mkdtempSync(tmpdir() + sep);
 
 test('onezip: extract: no args', (t) => {
     const [error] = tryCatch(extract);
+    
     t.equal(error.message, 'from should be a string!', 'should throw when no args');
     t.end();
 });
 
 test('onezip: extract: to', (t) => {
     const [error] = tryCatch(extract, 'hello');
+    
     t.equal(error.message, 'to should be string or object!', 'should throw when no to');
     t.end();
 });
@@ -105,26 +111,40 @@ test('onezip: extract: dir', async (t) => {
     t.end();
 });
 
+test('onezip: extract: dir + file: no error', async (t) => {
+    const to = tmp();
+    const from = join(__dirname, 'fixture', 'dir+file.zip');
+    const extracter = extract(from, to);
+    
+    await once(extracter, 'end');
+    
+    const pathUnpacked = join(to, 'file1.txt');
+    const fileUnpacked = readFileSync(pathUnpacked, 'utf8');
+    
+    rimraf.sync(to);
+    
+    t.equal(fileUnpacked, '', 'should extract directory');
+    t.end();
+});
+
 test('onezip: exract: writeFile: error', async (t) => {
-    delete require.cache[require.resolve('..')];
-    delete require.cache[require.resolve('mkdirp')];
+    const mkdir = stub().throws(Error('Can not create directory!'));
+    const fs = require('fs/promises');
     
-    const mkdirp = require('mkdirp');
-    
-    require.cache[require.resolve('mkdirp')].exports = async () => {
-        throw Error('Can not create directory!');
-    };
-    
-    const {extract} = require('..');
+    mockRequire('fs/promises', {
+        ...fs,
+        mkdir,
+    });
+    const {extract} = reRequire('..');
     
     const to = tmpdir();
     const from = join(__dirname, 'fixture', 'dir.zip');
     
     const extracter = extract(from, to);
-    
     const [{message}] = await once(extracter, 'error');
     
-    require.cache[require.resolve('mkdirp')].exports = mkdirp;
+    stopAll();
+    
     t.equal(message, 'Can not create directory!', 'should not create directory');
     t.end();
 });
