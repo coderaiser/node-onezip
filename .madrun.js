21508b40c57b1da365cb4dd9d388934aaabbd5f1
coderaiser <coderaiser@cloudcmd.io>
2021-01-05 14:21:44 +0200

    chore(madrun) lint

--- .madrun.js
+++ .madrun.js
@@ -4,6 +4,8 @@ const {run} = require('madrun');
 
 module.exports = {
     'lint': () => 'putout .',
+    'fresh:lint': () => run('lint', '--fresh'),
+    'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
     'test': () => 'tape \'test/**/*.js\'',
     'coverage': () => 'nyc npm test',
