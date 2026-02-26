7730242266fca31e55641b704f3d33c499ac2b5b
coderiaser <coderaiser@cloudcmd.io>
2026-02-26 20:04:11 +0200

    fix: onezip: pack/extract

--- test/onezip.js
+++ test/onezip.js
@@ -1,10 +1,20 @@
 import {tryCatch} from 'try-catch';
 import {test} from 'supertape';
-import {onezip} from '../lib/onezip.js';
+import {onezip, pack, extract} from '../lib/onezip.js';
 
 test('onezip: no args', (t) => {
     const [error] = tryCatch(onezip);
     
     t.equal(error.message, 'operations could be "pack" or "extract" only!', 'should throw when bad operation');
+    t.end();
+});
+
+test('onezip: pack: exists', (t) => {
+    t.equal(onezip.pack, pack);
+    t.end();
+});
+
+test('onezip: extract: exists', (t) => {
+    t.equal(onezip.extract, extract);
     t.end();
 });
